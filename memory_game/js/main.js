59bd39398b8200bba9f3986e7534226e7601e64e
AQILAHRAJAB <aqilah@majulah.sg>
2019-06-17 18:52:35 +0800

    Added

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "king";
+var cardFour = "queen";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
