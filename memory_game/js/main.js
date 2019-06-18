6212fb9a959ca0a745123933d2256f5bf3ed5424
AQILAHRAJAB <aqilah@majulah.sg>
2019-06-18 22:06:01 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
-
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPLay = [];
+var cardOne = cards[1];
+var cardTwo = cards[4];
+cardsInPLay.push("cardOne");
+cardsInPLay.push("cardTwo");
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
+console.log(cardsInPLay.length);
+
+if (cardsInPLay.length === 2){ 
+ alert("You found a match!")
+	} else { 
+	  alert("Sorry, try again")
+};
+
+
